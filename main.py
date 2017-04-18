2fb95c2d0c6ff31abe2d02da1fb05015fbbce8b3
GunnarHorve <gunnarhorve@gmail.com>
2017-04-18 14:04:05 -0400

    observations recorded as a series of arrays

--- main.py
+++ main.py
@@ -51,15 +51,26 @@ def createTPLocs(my_mission, startPos, obsDims, numPillars, numPlatforms):
     stopX = x + numPillars + obsDims[0]*2*numPillars + 1
     stepX = obsDims[0] * 2 + 1
 
+    strtY = y - numPlatforms
+    stopY = y + numPlatforms + 1
+    stepY = 1
+
     strtZ = z - numPillars - obsDims[2]*2*numPillars
     stopZ = z + numPillars + obsDims[2]*2*numPillars + 1
     stepZ = obsDims[2] * 2 + 1
 
-    tplocs = []
-    for ex in range(strtX, stopX, stepX):
-        for why in range(-numPlatforms, numPlatforms + 1, 1):
-            for zee in range(strtZ, stopZ, stepZ):
-                tplocs.append((x+0.5, y + y*obsDims[1], z+0.5))
+    tplocs = [[[(0,0,0) for k in xrange(2*numPillars + 1)] for j in xrange(2*numPlatforms + 1)] for i in xrange(2*numPillars + 1)]
+
+    x = 0
+    for i in range(strtX, stopX, stepX):
+        y = 0
+        for j in range(strtY, stopY, stepY):
+            z = 0
+            for k in range(strtZ, stopZ, stepZ):
+                tplocs[x][y][z] = (i+.5, j, k+.5)
+                z = z + 1
+            y = y + 1
+        x = x + 1
 
     return tplocs
 
@@ -69,7 +80,9 @@ def initMalmo(mapping, startPos, obsDims, numPillars, numPlatforms):
     my_mission = MalmoPython.MissionSpec(missionXML, True)
     if(mapping):
         my_mission.setModeToSpectator()
-        createTPLocs(my_mission, startPos, obsDims, numPillars, numPlatforms)
+        tps = createTPLocs(my_mission, startPos, obsDims, numPillars, numPlatforms)
+    else:
+        tps = -1
 
     # create default malmo objects
     agent_host = MalmoPython.AgentHost()
@@ -101,42 +114,57 @@ def initMalmo(mapping, startPos, obsDims, numPillars, numPlatforms):
     print
     print "Mission running "
 
-    return (agent_host, world_state)
+    return (agent_host, world_state, tps)
 
-################## Boilerplate code above this point ##########################
+def samePoint(p1, p2):
+    return int(p1[0]) == int(p2[0]) and int(p1[1]) == int(p2[1]) and int(p1[2]) == int(p2[2])
+
+def waitForSensor(agent_host, tp):
+    sys.stdout.write("waiting for sensor at: {} {} {}".format(tp[0], tp[1], tp[2]))
+    while True:
+        sys.stdout.write(".")
+        time.sleep(0.1)
+
+        world_state = agent_host.getWorldState()
+        if not world_state.number_of_observations_since_last_state > 0: continue
+
+        # parse obsrvations
+        msg = world_state.observations[-1].text
+        obs = json.loads(msg)
+        if(samePoint((obs[u'XPos'], obs[u'YPos'], obs[u'ZPos']), tp)):
+            grid = obs.get(u'sliceObserver', 0)
+            return grid
+
+def makeMap(agent_host, world_state, tps):
+    # loop through all teleport locations
+    for i in range(len(tps)):
+        for j in range(len(tps[0])):
+            for k in range(len(tps[0][0])):
+                tp = tps[i][j][k]
+                agent_host.sendCommand("tp {} {} {}".format(tp[0], tp[1], tp[2]))
+                grid = waitForSensor(agent_host, tp)
+                sys.stdout.write(" {}\n".format(len(grid)))
+
+def runSearch(agent_host, world_state):
+    # agent_host.sendCommand("tp 100 100 100")
+    return
 
 def main():
     #world recording parameters
     startPos = [252.5, 68.0, -214.5, 90.0] # x,y,z,yaw
-    # startPos = [200.5, 100.0, 200.5, 90.0] #  +/+
-    # startPos = [-200.5, 100.0, -200.5, 90.0] # -/-
-    # startPos = [200.5, 100.0, -200.5, 90.0] # +/- --> x down, z down
-    # startPos = [-200.5, 100.0, 200.5, 90.0] # -/+
-
 
     obsDims = [5, 1, 5]
     mapping = True
-    numPillars = 1 #spaces observed = -O3--O2--O1--O2--O3-
-    numPlatforms = 1
+    numPillars = 2 #spaces observed = -O3--O2--O1--O2--O3-
+    numPlatforms = 3
 
     # Loading world, initializing malmo
-    agent_host, world_state = initMalmo(mapping, startPos, obsDims, numPillars, numPlatforms)
+    agent_host, world_state, tps = initMalmo(mapping, startPos, obsDims, numPillars, numPlatforms)
 
-    # Loop until mission ends:
-    while world_state.is_mission_running:
-        sys.stdout.write(".")
-        time.sleep(0.1)
-        world_state = agent_host.getWorldState()
-        # agent_host.sendCommand("tp 100 100 100")
+    if(mapping):
+        makeMap(agent_host, world_state, tps)
+    else:
+        runSearch(agent_host, world_state)
 
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
 main()
-
-    # for error in world_state.errors:
-    #     print "Error:",error.text
-    # if world_state.number_of_observations_since_last_state > 0:
-    #     msg = world_state.observations[-1].text
-    #     observations = json.loads(msg)
-    #     grid = observations.get(u'sliceObserver', 0)
-    #     print len(grid)
-    #     break
